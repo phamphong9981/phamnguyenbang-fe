2b900c7ba4adcc5197fec54f7403f93854103c29
Pham Phong <phamphong9981@gmail.com>
2025-09-09 06:11:00 +0000

    game

--- next.config.js
+++ next.config.js
@@ -17,6 +17,11 @@ const nextConfig = {
             { protocol: 'https', hostname: 'pub-17a73393d80349528c61ccd4ed8e3061.r2.dev', port: '', pathname: '/**' },
         ],
     },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
 }
 
 module.exports = nextConfig 
